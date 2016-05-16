60bd8d9ceb2d34ce9d059f255b4c91c3a35b4166
Ryan Johnson <ryj1023@yahoo.com>
2016-05-16 19:50:20 -0400

    "Final Commit"

--- js/app.js
+++ js/app.js
@@ -1,163 +1,147 @@
-//initialize variables
-'use strict';
-var secretNumber, 
-userGuess, 
-pastGuesses = [], 
-count,
-guessHtml, 
-userFeedback,
-alreadyGuessed,
-newButton,
-form ,
-input,
-feedback,
-countElement,
-guessList;
+function randomNumber(){
+	var random = Math.floor((Math.random() * 100) + 1);
+     	console.log(random);
+     	return random;
+     };
 
-$(document).ready(pageLoad);
+    function newGame(){
+      
+    count = 0;
+    pastGuesses = []
+    userGuess = 0;
+    random = randomNumber();
+    $("#feedback").text("Make your Guess!");
+    $('.numGuessed').html("").hide();
+    $('#count').html("0");  
+  }
 
- function pageLoad(){
+$(document).ready(function(){
+
 	
+	 random = randomNumber();
+	 pastGuesses = []
+	 count = 0;
+	 var guessList,
+	 guessHtml,
+	 userFeedback,
+	 countElement;
+	 userFeedback = $('#feedback');
+	 guessList = $('#guessList');
+	 countElement = $('#count');
+
+	 
+ 	
+$("a.new").click(function(){
+ 	
+	    newGame();
+ 			});
+			 
 	/*--- Display information modal box ---*/
-  	$('.what').click(function(){
-    	$('.overlay').fadeIn(1000);
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
   	});
+     
   	/*--- Hide information modal box ---*/
-  	$('a.close').click(function(){
-  		$('.overlay').fadeOut(1000);
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
   	});
 
-  	//fetch dom objects
-  	newButton = $('a.new');
-  	form = $('form');
-  	input = form.find('#userGuess');
-  	feedback = $('#feedback');
-  	countElement = $('#count');
-  	guessList = $('#guessList');
+	
 
-    //page load
-    newGame();
-    //event handlers
-    form.submit(function(event){
-      event.preventDefault();
-      getUserGuess();
-    });
-    newButton.click(newGame);
-}
+		/* Generate random number*/
+		
 
-//new game function
-function newGame(){
-	form.find('input[type=submit]').css('opacity','1');
-	resetVariables();
-	render();
-	generateNumber();
-}
+	/* Get user input*/
+    var userGuess; 
+    $('#userInput').submit(function (){
+    	
+    	
+	    userGuess = $('#userGuess').val();
+	    userGuess = parseInt(userGuess);
+	    event.preventDefault();
+	    $('#userGuess').val('');
+	    $('#userGuess').focus();
+	    console.log(feedback());
 
-//get the user guess
-function getUserGuess(){
-	//get the user guess
-	userGuess = input.val();
-	//reset input value
-	input.val('');
-	//focus on input for next guess
-	input.focus();
-	//ensure valid input
-	if(checkGuess()){return ;}
-	//generate feedback
-	generateFeedback();
-	//track the past user guesses
-	trackGuess();
-	//increment the count
-	guessCount();
-	//render changes to the page
-	render();
-}
-
-  	//check for valid input
-  	function checkGuess(){
+     	/* use random number and user guess to provide feedback */
+	    
+     function checkGuess(){
   		if(userGuess % 1 !== 0){
-  			alert('please input a number');
+  		alert('please enter a number');
+  		return true;
+  }
+  		if(userGuess < 0 || userGuess > 100){
+  			alert("please pick a number between 0 and 100");
   			return true;
   		}
-  		if(userGuess < 0 || userGuess > 101){
-  			alert('please choose a number between zero and 100');
-  			return true;
-  		}
-  		if(pastGuesses.length > 0){
-			$.each(pastGuesses,function(guess,value){
-				if(userGuess == value){
-					alreadyGuessed = true;
-				}
-			}); 
-		}
-		if(alreadyGuessed){
-			alreadyGuessed = false;
-			alert('You guessed this number already');
-			return true;
-		}
-    return false;
-	}
 
-//generate user feedback
-function generateFeedback(){
-	if(secretNumber == userGuess){
-		winner();
-	} else if(Math.abs(secretNumber - userGuess) < 10){
-		userFeedback = 'hot';
-	} else if(Math.abs(secretNumber - userGuess) < 20 && Math.abs(secretNumber - userGuess) > 9){
-		userFeedback = ' Kinda hot';
-	} else if(Math.abs(secretNumber - userGuess) < 30 && Math.abs(secretNumber - userGuess) > 19){
-		userFeedback = 'less than warm';
-	} else {
-		userFeedback = 'cold';
-	}
+  return false;
+
 }
 
-//keep track of the users past guesses
-function trackGuess(){
-	pastGuesses.push(userGuess);
-	guessHtml = '';
-	if(pastGuesses[0].length) {
-		$.each(pastGuesses,function(guess,value){
-			guessHtml += '<li>' + value + '</li>';
-		});
-	}
-}
+      function feedback(){
 
-//keep track of guess count
-function guessCount(){
-	count++;
-}
-
-	//page render function
-function render(){
-	guessList.html(guessHtml);
-	countElement.html(count);
-	feedback.html(userFeedback);
-}
-
-function winner(){
-	userFeedback = 'You Won. Click new game to play again';
-	form.find('input[type=submit]').css('opacity','0');
-}
-  	
-//generate secret number
-function generateNumber(){
-	secretNumber = Math.floor(Math.random()*100)+1;
-}
-
-//reset variable 
-function resetVariables(){
-	count = 0;
-	pastGuesses = [];
-	guessHtml='';
-	userGuess = '';
-	userFeedback = 'Make your Guess!';
-}
-  	
-  	
-
+      	  checkGuess();
+      	  trackGuesses();
+      	  
+      	  if(userGuess == random){
+  				alert("Congratulations! You Win! Lets play again!")
+    			$("#feedback").text("You win!")
+    			newGame();
+    		
+    		  		
+    	 }else if(Math.abs(random - userGuess) < 10){ 	 	
+    			
+    			$("#feedback").text("Very Hot")
+    		    count++;
   
+    		    
+    	}else if(Math.abs(random - userGuess) > 10 && Math.abs(random - userGuess) < 20){
+    		
+    			$("#feedback").text("Hot")
+    		    count++;
+    			
+    	}else if(Math.abs(random - userGuess) > 20 && Math.abs(random - userGuess) < 30){
+    		
+    			$("#feedback").text("Barely Hot")
+    		    count++;
+    			
+    	 }else {
+    	 	
+    			$("#feedback").text("Cold")
+    	      count++;
+
+
+    	    	
+    	 };
+          $("#count").text(count);
+			
+ 	};
+
+
+ 	function trackGuesses(){
+
+ 		if(userGuess % 1 === 0){
+ 		 pastGuesses.push(userGuess);
+ 		}
+ 		 $.each(pastGuesses, function (){
+ 		 $('#guessList').append('<li class="numGuessed"><span>' + userGuess + '</span></li>')	
+ 			});
+ 		  pastGuesses = [];
+
+ 		
+ 	}
+
+
+ 	
+
+
+ 
+	
+ 	
+	});
+});
 
 
 
